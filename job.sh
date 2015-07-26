06b2f8bd42cedb2330b4b96a48f1f1e0f4ebe774
kaushikyasaswy <kaus@seas.upenn.edu>
2015-07-26 12:50:33 -0400

    final changes

--- job.sh
+++
@@ -1,6 +0,0 @@
-cp ./divya_temp/*.svg ./public/images/
-cp ./divya_temp/*.png ./public/images/
-cp ./divya_temp/*.js ./public/javascripts/
-cp ./divya_temp/*.css ./public/stylesheets/
-cp ./divya_temp/*.jpg ./public/images/
-cp ./divya_temp/*.gif ./public/images/
